9cc8fda6f64ea4c2e1a714caf98da38d054d3488
Aseel Shareef <aseelsharyf@gmail.com>
2025-12-05 02:37:02 +0500

    cleanup

--- resources/views/components/sections/divider.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- resources/views/components/sections/divider.blade.php
-     Vertical divider line for between cards/sections
---}}
-@props([
-    'color' => 'outline-white',
-    'hideOnMobile' => true,
-])
-
-<div class="{{ $hideOnMobile ? 'hidden md:block' : '' }} w-0 self-stretch outline outline-1 outline-offset-[-0.5px] {{ $color }} shrink-0"></div>
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
-Route::get('/template', function () {
-    return view('template');
-});
